9d4d9c370e362075764d0973946f57eae3a91443
Chris Pulman <chris.pulman@yahoo.com>
2024-01-08 01:36:41 +0000

    Add base functions

---
+++ src/CrissCross.WPF.UI/MagicInterfaces/IControlAppBar.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Chris Pulman. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace CrissCross.WPF.UI
+{
+    /// <summary>
+    /// Enables control of AppBar on modernWindow.
+    /// </summary>
+    public interface IControlAppBar
+    {
+    }
+}
