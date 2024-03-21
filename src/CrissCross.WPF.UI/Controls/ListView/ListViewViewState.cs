fd54b28d4ede83fd2becaf54fcbcd56d750f1b56
Chris Pulman <chris.pulman@yahoo.com>
2024-03-21 11:08:24 +0000

    Update ListView

---
+++ src/CrissCross.WPF.UI/Controls/ListView/ListViewViewState.cs
@@ -0,0 +1,26 @@
+// Copyright (c) Chris Pulman. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+////   This file has been borrowed from Wpf-UI.
+
+//// This Source Code Form is subject to the terms of the MIT License.
+//// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+//// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+//// All Rights Reserved.
+
+namespace CrissCross.WPF.UI.Controls;
+
+/// <summary>
+/// ListViewViewState.
+/// </summary>
+public enum ListViewViewState
+{
+    /// <summary>
+    /// The default.
+    /// </summary>
+    Default,
+    /// <summary>
+    /// The grid view.
+    /// </summary>
+    GridView,
+}
