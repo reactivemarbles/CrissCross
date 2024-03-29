8c887ec144d588116d024435309a61c2ab6985e9
Chris Pulman <chris.pulman@yahoo.com>
2024-03-29 02:00:51 +0000

    Feature add CheckBoxModern

---
+++ src/CrissCross.WPF.UI/Controls/CheckBoxModern/CheckBoxResultEventArgs.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Chris Pulman. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace CrissCross.WPF.UI.Controls
+{
+    /// <summary>
+    /// Check Box Result.
+    /// </summary>
+    public class CheckBoxResultEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CheckBoxResultEventArgs"/> class.
+        /// </summary>
+        public CheckBoxResultEventArgs()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CheckBoxResultEventArgs"/> class.
+        /// </summary>
+        /// <param name="userClicked">if set to <c>true</c> [user clicked].</param>
+        /// <param name="result">if set to <c>true</c> [result].</param>
+        public CheckBoxResultEventArgs(bool userClicked, bool result)
+        {
+            UserClicked = userClicked;
+            Checked = result;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="CheckBoxResultEventArgs"/> is checked.
+        /// </summary>
+        /// <value><c>true</c> if checked; otherwise, <c>false</c>.</value>
+        public bool Checked { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether [user clicked].
+        /// </summary>
+        /// <value><c>true</c> if [user clicked]; otherwise, <c>false</c>.</value>
+        public bool UserClicked { get; set; }
+    }
+}
---
+++ src/CrissCross.WPF.UI/Controls/CheckBoxModern/DockToTextAlignmentConverter.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Chris Pulman. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace CrissCross.WPF.UI.Controls
+{
+    /// <summary>
+    /// Dock To Text Alignment Converter.
+    /// </summary>
+    public class DockToTextAlignmentConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts the specified value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="targetType">Type of the target.</param>
+        /// <param name="parameter">The parameter.</param>
+        /// <param name="culture">The culture.</param>
+        /// <returns>TextAlignment value.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Dock dock)
+            {
+                switch (dock)
+                {
+                    case Dock.Left:
+                        return TextAlignment.Left;
+
+                    case Dock.Right:
+                        return TextAlignment.Right;
+
+                    case Dock.Top:
+                    case Dock.Bottom:
+                        return TextAlignment.Center;
+                }
+            }
+
+            return TextAlignment.Left;
+        }
+
+        /// <summary>
+        /// Converts the back.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="targetType">Type of the target.</param>
+        /// <param name="parameter">The parameter.</param>
+        /// <param name="culture">The culture.</param>
+        /// <returns>Dock value.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TextAlignment textAlignment)
+            {
+                switch (textAlignment)
+                {
+                    case TextAlignment.Left:
+                        return Dock.Left;
+
+                    case TextAlignment.Right:
+                        return Dock.Right;
+                }
+            }
+
+            return Dock.Left;
+        }
+    }
+}
