c73a5450249cc72f95ae47697a219bf51776885d
Chris Pulman <chris.pulman@yahoo.com>
2024-08-01 18:08:36 +0100

    Add new basic controls in the same namespace
    Simplification of namespaces when creating a project

---
+++ src/CrissCross.WPF.UI/Controls/StackPanel/StackPanel.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2019-2024 ReactiveUI Association Incorporated. All rights reserved.
+// ReactiveUI Association Incorporated licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+namespace CrissCross.WPF.UI.Controls
+{
+    /// <summary>
+    /// Extended <see cref="System.Windows.Controls.StackPanel"/>.
+    /// </summary>
+    /// <seealso cref="System.Windows.Controls.StackPanel" />
+    public class StackPanel : System.Windows.Controls.StackPanel;
+}
---
+++ src/CrissCross.WPF.UI/Controls/TreeView/TreeView.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) 2019-2024 ReactiveUI Association Incorporated. All rights reserved.
+// ReactiveUI Association Incorporated licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+namespace CrissCross.WPF.UI.Controls;
+
+/// <summary>
+/// Extended <see cref="System.Windows.Controls.TreeView"/>.
+/// </summary>
+/// <seealso cref="System.Windows.Controls.TreeView" />
+public class TreeView : System.Windows.Controls.TreeView;
