14b98923716e8ee6cf87f10f228f0f8d6f980f1c
Chris Pulman <chris.pulman@yahoo.com>
2025-12-06 15:26:40 +0000

    Add AutoSuggestBox sample and new button styles
    Introduces an AutoSuggestBox sample with fruit suggestions to the input page, including new styles for AutoSuggestBox, BezelButton, and GelButton. Updates Calendar, CheckBox, CheckBoxModern, and HexColorTextBox themes for improved appearance and consistency. Index.axaml now includes the new button styles.

--- src/CrissCross.Avalonia.UI.Gallery/ViewModels/InputPageViewModel.cs
+++ src/CrissCross.Avalonia.UI.Gallery/ViewModels/InputPageViewModel.cs
@@ -2,6 +2,8 @@
 // ReactiveUI Association Incorporated licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
+using System.Collections.Generic;
+
 namespace CrissCross.Avalonia.UI.Gallery.ViewModels;
 
 /// <summary>
@@ -14,4 +16,41 @@ public class InputPageViewModel : RxObject
     /// </summary>
     public InputPageViewModel() =>
         this.BuildComplete(() => DisplayName = "Input Controls");
+
+    /// <summary>
+    /// Gets the sample suggestions for the AutoSuggestBox.
+    /// </summary>
+    public IEnumerable<string> SampleSuggestions { get; } =
+    [
+        "Apple",
+        "Apricot",
+        "Avocado",
+        "Banana",
+        "Blackberry",
+        "Blueberry",
+        "Cherry",
+        "Coconut",
+        "Cranberry",
+        "Date",
+        "Dragonfruit",
+        "Fig",
+        "Grape",
+        "Grapefruit",
+        "Guava",
+        "Kiwi",
+        "Lemon",
+        "Lime",
+        "Mango",
+        "Melon",
+        "Orange",
+        "Papaya",
+        "Peach",
+        "Pear",
+        "Pineapple",
+        "Plum",
+        "Pomegranate",
+        "Raspberry",
+        "Strawberry",
+        "Watermelon"
+    ];
 }
