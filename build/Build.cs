a8e452577b97f1253fa979289ddc96817c914017
Chris Pulman <chris.pulman@yahoo.com>
2026-07-12 09:33:05 +0100

    Simplify BuildOnly workflow for branch pushes
    Refactors the BuildOnly GitHub Actions workflow to run on non-main push events instead of pull requests, and removes generated boilerplate and versioning/JDK/workload steps. The job now focuses on .NET setup (including MSBuild), compile via `build.cmd Compile`, then runs tests with Cobertura coverage output and uploads coverage artifacts for short-term retention.

--- build/Build.cs
+++ build/Build.cs
@@ -8,40 +8,23 @@ using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.MSBuild;
-using Nuke.Common.Tools.NerdbankGitVersioning;
 using Serilog;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
-////[GitHubActions(
-////    "BuildOnly",
-////    GitHubActionsImage.WindowsLatest,
-////    OnPushBranchesIgnore = new[] { "master" },
-////    FetchDepth = 0,
-////    InvokedTargets = new[] { nameof(Compile) })]
-////[GitHubActions(
-////    "BuildDeploy",
-////    GitHubActionsImage.WindowsLatest,
-////    OnPushBranches = new[] { "master" },
-////    FetchDepth = 0,
-////    ImportSecrets = new[] { nameof(NuGetApiKey) },
-////    InvokedTargets = new[] { nameof(Compile), nameof(Deploy) })]
 sealed partial class Build : NukeBuild
 {
-    public static int Main() => Execute<Build>(x => x.Pack);
+    public static int Main() => Execute<Build>(x => x.Compile);
 
     private static AbsolutePath SolutionFile => RootDirectory / "src" / "CrissCross.slnx";
 
-    [GitRepository] readonly GitRepository Repository;
     readonly Solution Solution = SolutionFile.ReadSolution();
-    [NerdbankGitVersioning] readonly NerdbankGitVersioning NerdbankVersioning;
-    [Parameter][Secret] readonly string NuGetApiKey;
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
     AbsolutePath PackagesDirectory => RootDirectory / "output";
 
     Target Print => _ => _
-        .Executes(() => Log.Information("NerdbankVersioning = {Value}", NerdbankVersioning.NuGetPackageVersion));
+        .Executes(() => Log.Information("MinVerVersionOverride = {Value}", Environment.GetEnvironmentVariable("MinVerVersionOverride") ?? "<auto>"));
 
     Target Clean => _ => _
         .Before(Restore)
@@ -69,42 +52,4 @@ sealed partial class Build : NukeBuild
                 .SetProjectFile(Solution)
                 .SetConfiguration(Configuration)
                 .SetNoRestore(true)));
-
-    Target Pack => _ => _
-    .DependsOn(Compile)
-    .Produces(PackagesDirectory / "*.nupkg")
-    .Executes(() =>
-    {
-        if (Repository.IsOnMainOrMasterBranch())
-        {
-            var packableProjects = Solution.GetPackableProjects();
-            foreach (var project in packableProjects!)
-            {
-                Log.Information("Packing {Project}", project.Name);
-            }
-
-            DotNetPack(settings => settings
-                .SetConfiguration(Configuration)
-                .SetNoBuild(true)
-                .SetVersion(NerdbankVersioning.NuGetPackageVersion)
-                .SetOutputDirectory(PackagesDirectory)
-                .CombineWith(packableProjects, (packSettings, project) =>
-                    packSettings.SetProject(project)));
-        }
-    });
-
-    Target Deploy => _ => _
-    .Requires(() => NuGetApiKey)
-    .Executes(() =>
-    {
-        if (Repository.IsOnMainOrMasterBranch())
-        {
-            DotNetNuGetPush(settings => settings
-                        .SetSource(this.PublicNuGetSource())
-                        .SetSkipDuplicate(true)
-                        .SetApiKey(NuGetApiKey)
-                        .CombineWith(PackagesDirectory.GlobFiles("*.nupkg"), (s, v) => s.SetTargetPath(v)),
-                    degreeOfParallelism: 5, completeOnFailure: true);
-        }
-    });
 }
